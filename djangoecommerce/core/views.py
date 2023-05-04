e915ffce8a9c0b7019d8a3981e09a9f4664354d8
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-05-04 20:13:37 -0300

    montando template

--- djangoecommerce/core/views.py
+++ djangoecommerce/core/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Hello Word');
+    texts = ['Lore Ipsum teste doloer sit ame', 'conserto dadiel edit']
+    context = {
+        'title': 'Django E-Commerce',
+        'texts': texts
+    }
+    return render(request, 'index.html', context);
