664c8581a3ab3d95526159d7397d4dbe4bdac9a4
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-05-05 22:32:18 -0300

    Criando arquivo de test

--- djangoecommerce/core/tests/test_views.py
+++ djangoecommerce/core/tests/test_views.py
@@ -1,7 +1,19 @@
 from django.test import TestCase, Client
 
 class IndexViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def tearDown(self):
+        pass
+
+
     def test_status_code(self):
         client = Client()
         response = client.get('/')
         self.assertEquals(response.status_code, 200)
+
+    def test_template_used(self):
+        client = Client()
+        response = client.get('/')
+        self.assertTemplateUsed(response, 'index.html')
