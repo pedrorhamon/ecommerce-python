b24249613d59831eec83b1e9fd479d28cd0421eb
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-05-06 17:47:25 -0300

    adicionando o ';'

--- djangoecommerce/core/tests/test_views.py
+++ djangoecommerce/core/tests/test_views.py
@@ -1,21 +1,21 @@
-from django.test import TestCase, Client
-from django.urls import reverse
+from django.test import TestCase, Client;
+from django.urls import reverse;
 
 class IndexViewTestCase(TestCase):
     def setUp(self):
-        self.client = Client()
-        self.url = reverse('index')
+        self.client = Client();
+        self.url = reverse('index');
 
     def tearDown(self):
-        pass
+        pass;
 
 
     def test_status_code(self):
-        client = Client()
-        response = client.get('/')
-        self.assertEquals(response.status_code, 200)
+        client = Client();
+        response = client.get('/');
+        self.assertEquals(response.status_code, 200);
 
     def test_template_used(self):
-        client = Client()
-        response = client.get('/')
-        self.assertTemplateUsed(response, 'index.html')
+        client = Client();
+        response = client.get('/');
+        self.assertTemplateUsed(response, 'index.html');
