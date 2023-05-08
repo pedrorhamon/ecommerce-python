89b96c96e6902530db1c2a3d1bbeb0fc1a65807a
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-05-08 20:07:39 -0300

    criando arquivo de configuração

--- djangoecommerce/djangoecommerce/settings.py
+++ djangoecommerce/djangoecommerce/settings.py
@@ -128,3 +128,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
