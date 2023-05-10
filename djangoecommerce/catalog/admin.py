162418f85a625ff097b4ae8d518d6797a25a096e
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-05-10 20:33:11 -0300

    adicionando o filter

--- djangoecommerce/catalog/admin.py
+++ djangoecommerce/catalog/admin.py
@@ -6,11 +6,13 @@ class CategoryAdmin(admin.ModelAdmin):
 
     list_display = ['name', 'slug', 'created', 'modified'];
     search_fields = ['name', 'slug'];
+    list_filter = ['created', 'modified'];
 
 class ProductAdmin(admin.ModelAdmin):
 
     list_display = ['name', 'slug', 'category','created', 'modified'];
     search_fields = ['name', 'slug', 'category__name'];
+    list_filter = ['created', 'modified'];
 
 
 admin.site.register(Category, CategoryAdmin);
