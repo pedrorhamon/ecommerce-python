3cf44e42ff5447daee9cd66a73a84f5cc3b479ff
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-05-10 20:31:42 -0300

    customizando o admin

--- djangoecommerce/catalog/admin.py
+++ djangoecommerce/catalog/admin.py
@@ -2,6 +2,17 @@ from django.contrib import admin
 
 from .models import Product, Category
 
-admin.site.register([Category])
-admin.site.register([Product])
+class CategoryAdmin(admin.ModelAdmin):
+
+    list_display = ['name', 'slug', 'created', 'modified'];
+    search_fields = ['name', 'slug'];
+
+class ProductAdmin(admin.ModelAdmin):
+
+    list_display = ['name', 'slug', 'category','created', 'modified'];
+    search_fields = ['name', 'slug', 'category__name'];
+
+
+admin.site.register(Category, CategoryAdmin);
+admin.site.register(Product, ProductAdmin);
 
--- djangoecommerce/catalog/models.py
+++ djangoecommerce/catalog/models.py
@@ -16,6 +16,9 @@ class Category(models.Model):
         verbose_name_plural = 'Categorias'
         ordering = ['name']
 
+    def __str__(self):
+        return self.name;
+
 
 class Product(models.Model):
 
