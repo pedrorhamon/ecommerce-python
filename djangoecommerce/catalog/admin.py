9951ef146d76cbd8eb75dcfd6f37b00e946961ad
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-05-11 23:35:59 -0300

    parametrizando dados

--- djangoecommerce/catalog/admin.py
+++ djangoecommerce/catalog/admin.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
+from django.contrib import admin;
 
-from .models import Product, Category
+from .models import Product, Category;
 
 class CategoryAdmin(admin.ModelAdmin):
 
