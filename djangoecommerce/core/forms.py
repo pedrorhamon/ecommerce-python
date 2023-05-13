5df33f754d919b398b5ca123081d3b57f911b2c8
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-05-13 16:37:33 -0300

    Criando primeiro form

---
+++ djangoecommerce/core/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+class ContactForm(forms.Form): 
+
+    name = forms.CharField(label='Nome', required=True);
+    email = forms.EmailField(label='E-mail', required=True);
+    mensagem = forms.CharField(label='Mensagem', widget=forms.Textarea);
