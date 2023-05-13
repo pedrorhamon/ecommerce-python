9beb1bb3a68b5f9347e79bc0fd586d07f84b42ac
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-05-13 16:31:21 -0300

    retirando hash na url

--- djangoecommerce/catalog/urls.py
+++ djangoecommerce/catalog/urls.py
@@ -6,6 +6,6 @@ app_name = 'catalog'
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
-    path('(?P<slug>[\w_-]+)', views.category, name='category'),
-    path('produtos/(?P<slug>[\w_-]+)', views.product, name='product'),
+    path('', views.category, name='category'),
+    path('produtos/', views.product, name='product'),
 ]
