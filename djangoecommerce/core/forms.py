bc13de6412edaa272459f6a7afbdbaa90c8f3d5a
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-05-13 16:54:30 -0300

    Adicionando o required

--- djangoecommerce/core/forms.py
+++ djangoecommerce/core/forms.py
@@ -4,4 +4,4 @@ class ContactForm(forms.Form):
 
     name = forms.CharField(label='Nome', required=True);
     email = forms.EmailField(label='E-mail', required=True);
-    mensagem = forms.CharField(label='Mensagem', widget=forms.Textarea);
+    mensagem = forms.CharField(label='Mensagem', widget=forms.Textarea, required=True);
