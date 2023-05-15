5e733a8d466368cbeac2ed28128ec30886a47743
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-05-15 20:03:39 -0300

    Resolvendo problema de validações

--- djangoecommerce/djangoecommerce/urls.py
+++ djangoecommerce/djangoecommerce/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('contato/', views.contact, name='contact'),
     # path('produto/', views.product, name='product'),
-    path('catalogo/', include('catalog.urls', 'catalog')),
+    path('produtos/', include('catalog.urls', 'catalog')),
     path('admin/', admin.site.urls),
 ]
