56770e739a1ed42404e8472226964af87ed75e0b
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-05-21 20:15:36 -0300

    utilizando o email para logar

--- djangoecommerce/djangoecommerce/settings.py
+++ djangoecommerce/djangoecommerce/settings.py
@@ -142,6 +142,10 @@ LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_URL = 'logout'
 AUTH_USER_MODEL = 'accounts.User'
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'accounts.backends.ModelBackend',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
